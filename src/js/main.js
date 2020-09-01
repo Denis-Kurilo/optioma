b73d46174e27094e8fc5a5c8374d205c2caaa7cb
Kurilo Denis <admin@mail.ru>
2020-09-01 17:09:42 +0300

    tabs-slider

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import popup from './modules/popup';
 import scrollDown from './modules/scrollDown';
 import maskPhone from './modules/maskPhone';
 import validationForm from './modules/validationForm';
+import repair from './modules/repairTabs';
 // import sendForm from './modules/sendForm';
 
 
@@ -17,4 +18,5 @@ scrollDown();
 popup();
 maskPhone();
 validationForm();
+repair();
 // sendForm();
---
+++ src/js/modules/repairTabs.js
@@ -0,0 +1,68 @@
+const repair = () => {
+	const	navList = document.querySelector('.nav-list'),
+	  repairSlider = document.querySelectorAll('.tabs-slider>img'),
+		repairTypesNavItem = document.querySelectorAll('.repair-types-nav__item'),
+		tabsSlider = document.querySelectorAll('.tabs-slider');
+
+	const toggleTabContent = (index) => {
+		for(let i = 0; i < tabsSlider.length; i++){
+			if(index === i){
+				repairTypesNavItem[i].classList.add('active');
+				tabsSlider[i].classList.remove('hidden');
+			}else{
+				repairTypesNavItem[i].classList.remove('active');
+				tabsSlider[i].classList.add('hidden');
+			}
+		}
+	};
+
+navList.addEventListener('click', (e) => {
+	let target = e.target;
+	if(target.classList.contains('repair-types-nav__item')){
+		repairTypesNavItem.forEach((item, i) => {
+			if(item === target){
+				toggleTabContent(i);
+				console.log(tabsSlider[i])
+			}
+		});
+	}
+});	
+
+
+	
+	const sliders = () => {
+		const sliderContent = document.querySelector('.repair-types-slider-wrap'),
+				slider = document.querySelectorAll('.slider'),
+				sliderArrow = document.querySelectorAll('.slider-arrow');
+		let currentSlide = 0;
+
+		const startSlide = (index) => {
+			sliderArrow.forEach((elem) => {
+				elem.addEventListener('click', (e) => {
+					let target = e.target;
+					console.log(target)
+					if(target.classList.contains('slider-arrow_right') || target.classList.contains('next')){
+						slider[currentSlide].classList.remove('item-active');
+						currentSlide++;
+						if(currentSlide >= slider.length){
+							currentSlide = 0;
+						}
+						slider[currentSlide].classList.add('item-active');
+					}else if(target.classList.contains('slider-arrow_left') || target.classList.contains('prev')){
+						if(currentSlide - 1 == -1){
+							currentSlide = slider.length -1;
+						}else{
+							slider[currentSlide].classList.remove('item-active');
+							currentSlide--;
+							slider[currentSlide].classList.add('item-active');
+						}
+					}
+				});
+			});
+		}
+		startSlide();
+	};
+	sliders();
+};
+
+export default repair;
