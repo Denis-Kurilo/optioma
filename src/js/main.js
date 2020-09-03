d7b09e7e2dc0803016f4f7897afb0281f508591c
Kurilo Denis <admin@mail.ru>
2020-09-03 17:11:37 +0300

    portfolio swiper

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 //plagin
 import GLightbox from 'glightbox';
-
+import Swiper, {Navigation} from 'swiper';
 
 import map from './modules/map';
 import asd from './modules/func';
@@ -12,6 +12,10 @@ import maskPhone from './modules/maskPhone';
 import validationForm from './modules/validationForm';
 import repair from './modules/repairTabs';
 // import sendForm from './modules/sendForm';
+
+
+// import Swiper JS
+
 
 
 
@@ -32,4 +36,19 @@ const lightbox = GLightbox ({
 	 	touchNavigation: true,
     loop: true
 }) ; 
+
+
+Swiper.use([Navigation]);
+const swiper = new Swiper('.portfolio__slider-inner', {
+	// direction: 'vertical',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: '.slider-arrow_right',
+    prevEl: '.slider-arrow_left',
+  },
+});
+
+
 
